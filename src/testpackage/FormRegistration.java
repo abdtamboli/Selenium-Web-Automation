4906387f5169e9a19b4e3205e2b1368c95f900d3
abdtamboli <69687553+abdtamboli@users.noreply.github.com>
2023-03-26 21:08:05 +0530

    Update FormRegistration.java

--- src/testpackage/FormRegistration.java
+++ src/testpackage/FormRegistration.java
@@ -20,13 +20,13 @@ public class FormRegistration {
 		driver.manage().window().maximize();
 		
 		WebElement email = driver.findElement(By.name("em"));
-		email.sendKeys("abhitamboli1997@gmail.com");
+		email.sendKeys("abhitamboli222@gmail.com");
 		
 		WebElement password = driver.findElement(By.name("pw1"));
-		password.sendKeys("Abd@12345");
+		password.sendKeys("xxxxx");
 		
 		WebElement Confirmpassword = driver.findElement(By.name("pw2"));
-		Confirmpassword.sendKeys("Abd@12345");
+		Confirmpassword.sendKeys("xxxxx");
 		
 		WebElement uncheck = driver.findElement(By.xpath("//*[@id='join-light']/form/div/div[4]/label/span"));
 		uncheck.click();
@@ -48,7 +48,7 @@ public class FormRegistration {
 		Loginemail.sendKeys("abhitamboli@gmail.com");
 		
 		WebElement Loginpassword = driver.findElement(By.name("pw"));
-		Loginpassword.sendKeys("Abd@12345");
+		Loginpassword.sendKeys("xxx");
 		
 		WebElement Loginnbtn = driver.findElement(By.xpath("//*['@id=join-login']/form/div[4]/button[2]/span"));
 		Loginnbtn.click(); 
